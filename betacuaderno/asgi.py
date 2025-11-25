ccdeb2a3dc403fc823785ccbe22b6940be93fc24
Constanza Schachtebeck <c.schachtebeckcurifu@uandresbello.edu>
2025-11-25 18:16:09 -0300

    Se modificó la denominación cuadernop a la nueva betacuaderno

--- betacuaderno/asgi.py
+++ betacuaderno/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cuadernop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'betacuaderno.settings')
 
 application = get_asgi_application()
